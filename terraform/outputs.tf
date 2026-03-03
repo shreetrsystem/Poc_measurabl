ad241d2966fd42b22a6e088a8cefa16289b519ca
shreetrsystem <shreetbhatnagar123@gmail.com>
2026-03-03 12:40:15 +0530

    added poc code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "lambda_url" {
-  value = aws_lambda_function_url.hello_world_url.function_url
+  # value = aws_lambda_function_url.hello_world_url.function_url
+  value = module.hello_world_lambda.lambda_function_url
 }
